b17629fdbae9da8a4e0fa860718542f78c983b6b
Souradeep321 <souradeephazra93@gmail.com>
2025-12-16 18:54:14 +0530

    feat(auth): integrate Clerk and organize project structure
    - Add Clerk authentication setup
    - Create initial folder structure
    - Add images and SVG assets to public directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const ibmPlexSans = IBM_Plex_Sans({
+  variable: "--font-ibm-plex-sans",
   subsets: ["latin"],
 });
 
@@ -23,12 +24,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={` ${ibmPlexSans.variable} ${geistSans.variable} antialiased dark`}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+
   );
 }
